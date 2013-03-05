f8e764d6630750f88f8725a1f3925aedc60870ea
ge-or-gi <georgi.mollov@gmail.com>
2013-03-05 21:22:50 +0200

    ъпдейт просто никакви промени

---
+++ LINQ-LAMBDA-ExtensionMetods/DelegateHomework/DummyClas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHomework
+{
+    class DummyClas
+    {
+
+
+
+        public void CallDelegate(string mesage)
+        {
+            Console.WriteLine(mesage);
+        }
+    }
+}
---
+++ LINQ-LAMBDA-ExtensionMetods/DelegateHomework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHomework
+{
+
+    public delegate void TestDelegate(string mesage);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DummyClas myDummyClass = new DummyClas();
+
+            TestDelegate td = new TestDelegate(myDummyClass.CallDelegate);
+
+            td("georgi");
+
+            
+
+
+        }
+
+        
+    }
+}
